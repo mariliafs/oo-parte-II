31d6213ccd9908e0260d209e39acf28141581f69
mariliafernandes <f.mariliafernandes@gmail.com>
2022-06-24 13:01:06 -0300

    implementação de herança e polimorfismo

--- modelo.py
+++ modelo.py
@@ -1,28 +1,45 @@
-class Filme:
+class Programa:
+    def __init__(self, nome, ano):
+        self._nome = nome.title()
+        self.ano = ano
+        self._likes = 0
+
+    @property
+    def likes(self):
+        return self._likes
+
+    def dar_like(self):
+        self._likes += 1
+
+    @property
+    def nome(self):
+        return self._nome
+
+    @nome.setter
+    def nome(self, novo_nome):
+        self._nome = novo_nome.title()
+
+class Filme(Programa):
     def __init__(self, nome, ano, duracao):
-        self.nome = nome.title()
-        self.ano = ano
+        super().__init__(nome,ano)
         self.duracao = duracao
-        self.likes = 0
 
-    def dar_like(self):
-        self.likes += 1
-
-class Serie:
+class Serie(Programa):
     def __init__(self, nome, ano, temporadas):
-        self.nome = nome.title()
-        self.ano = ano
+        super().__init__(nome,ano)
         self.temporadas = temporadas
-        self.likes = 0
-
-    def dar_like(self):
-        self.likes += 1
 
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
 vingadores.dar_like()
-print(f'Nome: {vingadores.nome} - Ano: {vingadores.ano} - Temporadas: {vingadores.duracao} - Likes: {vingadores.likes}')
+print(f'{vingadores.nome} - {vingadores.duracao}: {vingadores.likes}')
 
 atlanta = Serie('atlanta', 2018, 2)
 atlanta.dar_like()
 atlanta.dar_like()
-print(f'Nome: {atlanta.nome} - Ano: {atlanta.ano} - Temporadas: {atlanta.temporadas} - Likes: {atlanta.likes}')
+print(f'{atlanta.nome} - {atlanta.temporadas}: {atlanta.likes}')
+
+filmes_e_series = [vingadores,atlanta]
+
+for programa in filmes_e_series:
+    detalhes = programa.duracao if hasattr(programa,'duracao') else programa.temporadas
+    print(f'{programa.nome} - {detalhes} - {programa.likes}')
