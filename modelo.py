0f839d016192a9e89f946927301ea1b84ee102aa
mariliafernandes <f.mariliafernandes@gmail.com>
2022-06-24 18:01:07 -0300

    avançando no polimorfismo

--- modelo.py
+++ modelo.py
@@ -19,27 +19,36 @@ class Programa:
     def nome(self, novo_nome):
         self._nome = novo_nome.title()
 
+    def __str__(self):
+        return f'{self._nome} - {self.ano} - {self._likes} Likes'
+
 class Filme(Programa):
     def __init__(self, nome, ano, duracao):
         super().__init__(nome,ano)
         self.duracao = duracao
+
+    def __str__(self):
+        return f'{self._nome} - {self.ano} - {self.duracao} min - {self._likes} Likes'
 
 class Serie(Programa):
     def __init__(self, nome, ano, temporadas):
         super().__init__(nome,ano)
         self.temporadas = temporadas
 
+    def __str__(self):
+        return f'{self._nome} - {self.ano} - {self.temporadas} temporadas - {self._likes} Likes'
+
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
 vingadores.dar_like()
-print(f'{vingadores.nome} - {vingadores.duracao}: {vingadores.likes}')
+
 
 atlanta = Serie('atlanta', 2018, 2)
 atlanta.dar_like()
 atlanta.dar_like()
-print(f'{atlanta.nome} - {atlanta.temporadas}: {atlanta.likes}')
+
 
 filmes_e_series = [vingadores,atlanta]
 
 for programa in filmes_e_series:
-    detalhes = programa.duracao if hasattr(programa,'duracao') else programa.temporadas
-    print(f'{programa.nome} - {detalhes} - {programa.likes}')
+    print(programa)
+
